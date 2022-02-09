eeaf583ba809197fec3896a5b36ac536e38a4f69
Sushobhit Srivastava <sushobhitofficial@gmail.com>
2022-02-09 15:39:25 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom"
+import HomePage from './Pages/HomePage';
+import Speaker from "./Pages/Speaker"
+import ContactUs from "./Pages/ContactUs"
+import Presentation from "./Pages/Presentation"
+import Workshop from "./Pages/Workshop"
+import Toolbar from "./Components/Toolbar"
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Toolbar />
+      <Routes>
+        <Route path='/' exact element={<HomePage />} />
+        <Route path='/presentation' exact element={<Presentation />} />
+        <Route path='/speaker' exact element={<Speaker />} />
+        <Route path='/workshop' exact element={<Workshop />} />
+        <Route path='/contact' exact element={<ContactUs />} />
+      </Routes>
     </div>
   );
 }
