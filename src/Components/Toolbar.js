50446a462be28f7b0d174540044e8d572d8594ec
Sushobhit Srivastava <sushobhitofficial@gmail.com>
2022-02-09 19:55:41 +0530

    Coming Soon Made

--- src/Components/Toolbar.js
+++ src/Components/Toolbar.js
@@ -1,10 +1,32 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
+import Logo from "../Assets/Logo.jpg"
+import "./Toolbar.css"
 
 class Toolbar extends React.Component {
     render() {
         return (
-            <div>
-
+            <div style={{ height: "125px", display: "flex", justifyContent: "space-between" }}>
+                <div>
+                    <img src={Logo} style={{ height: "125px" }} />
+                </div>
+                {/* <div style={{ display: "flex", alignSelf: "center" }}>
+                    <NavLink className="menu-contents ">
+                        HOME
+                    </NavLink>
+                    <NavLink className="menu-contents">
+                        PRESENTATION
+                    </NavLink>
+                    <NavLink className="menu-contents">
+                        SPEAKERS
+                    </NavLink>
+                    <NavLink className="menu-contents">
+                        WORKSHOP
+                    </NavLink>
+                    <NavLink className="menu-contents">
+                        CONTACT US
+                    </NavLink>
+                </div> */}
             </div>
         )
     }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,21 @@
 import React from "react"
+import Home from "../Assets/Home-illustration.png"
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-
+            <div style={{ display: "flex", justifyContent: "space-around" }}>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                    <div style={{ textAlign: "center", fontSize: "32px", color: "var(--blue-color)", fontWeight: "900" }}>
+                        ENGAGING MINDS, <br />EMPOWERING THE FUTURE
+                    </div>
+                    <div style={{ color: "var(--yellow-color)", width: "400px", textAlign: "center", fontWeight: "600" }}>
+                        COMING SOON
+                    </div>
+                </div>
+                <div>
+                    <img src={Home} style={{ width: "500px" }} />
+                </div>
             </div>
         )
     }
